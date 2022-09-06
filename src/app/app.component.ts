3ff80e0d23ee6e5479bea5996761199fa86a9e20
Raamishshah010 <raamishshah010@gmail.com>
2022-09-06 18:25:18 +0500

    Responsiveness Left

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+// import { OwlOptions } from 'ngx-owl-carousel-o';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mdb-angular-ui-kit-free';
+  imgSrc = './assets/titllebottom.png'
+}
